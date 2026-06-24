96e1f3cf311a7b9503c75798d9d5e19aab70e7a8
mvgum <gunerov0@yandex.ru>
2026-06-24 10:04:58 +0500

    Refactor static files to use /static endpoint

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
 import os
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-
 template_folder = os.path.join(root_dir, "templates")
-js_directory = os.path.join(template_folder, "js")
-css_directory = os.path.join(template_folder, "css")
-images_directory = os.path.join(template_folder, "images")
+static_folder = os.path.join(root_dir, "static")
 
 app = Flask(__name__, template_folder=template_folder)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route("/js/<path:path>")
-def send_js(path):
-    return send_from_directory(js_directory, path)
-
-
-@app.route("/css/<path:path>")
-def send_css(path):
-    return send_from_directory(css_directory, path)
-
-
-@app.route("/images/<path:path>")
-def send_images(path):
-    return send_from_directory(images_directory, path)
-
+# Универсальный endpoint для статики
+@app.route("/static/<path:filename>")
+def send_static(filename):
+    # Проверяем, что файл существует
+    file_path = os.path.join(static_folder, filename)
+    if not os.path.exists(file_path):
+        abort(404)
+    return send_from_directory(static_folder, filename)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
